b3ff89ce73c8ae036878365c1f9f6dd5e20c0d8b
CreatorYuan <724991661@qq.com>
2024-04-20 13:01:34 +0800

    study flask base

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # pythonProject
 study python and creat pythonProject
+
+1. python 环境安装
+    
+   https://www.python.org/downloads/windows/
+
+2. IDE安装: pyCharm
+    
+    PyCharm Professional        --  Free 30-day trial
+    PyCharm Community Edition   --  Free, built on open source
+    https://www.jetbrains.com/pycharm/download/?section=windows
+
+3. 依赖包安装
+
+   pip install flask
+
+   pip install pymysql
+   
+   pip install flask-sqlalchemy
+
--- app.py
+++ app.py
@@ -1,14 +1,45 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from datetime import datetime
+
 
 app = Flask(__name__)
 
+
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
 # url: http[80]/https[443]://www.qq.com:443/path
 # url与视图： path与视图
+# 渲染模板 render_template("index.html")
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello CreatorYuan'
+    user = User(username="CreatorYuan", email="724991661@qq.com")
+    person = {
+        "username": "CY",
+        "email": "cy@qq.com"
+    }
+    return render_template("index.html", user=user, person=person)
+
+
+# 使用自定义过滤器
+def datetime_format(value, format="%Y-%d-%m %H:%M"):
+    return value.strftime(format)
+
+
+app.add_template_filter(datetime_format, "dformat")
+
+
+# Jinja2 内置过滤器, 使用加| , user.username|length
+# length abs default
+@app.route('/filter')
+def filter_demo():
+    user = User(username="CreatorYuan", email="724991661@qq.com")
+    mytime = datetime.now()
+    return render_template("filter.html", user=user, mytime=mytime)
 
 
 @app.route('/profile')
@@ -19,6 +50,52 @@ def profile():
 @app.route('/blog/list')
 def blog_list():
     return '我是博客列表'
+
+
+# 带参数的url: 将参数固定在path中 <blog_id>
+# <int:blog_id>: 指定参数类型
+# 参数可传可不传
+@app.route('/blog/<blog_id>')
+def blog_detail(blog_id):
+    return render_template("blog_detail.html", blog_id=blog_id, username="CreatorYuan")
+
+
+# /blog/list : 会给我返回第一页的数据
+# /blog/list?page=2 : 获取第二页的数据
+@app.route('/book/list')
+def book_list():
+    # arguments : 参数
+    # request.args : 类字典类型
+    page = request.args.get("page", default=1, type=int)
+    return f"你获取的是第{page}页的图书列表"
+
+
+@app.route('/control')
+def control_statement():
+    age = 32
+    books = [{
+        "name": "三国演义",
+        "author": "罗贯中"
+    }, {
+        "name": "水浒传",
+        "author": "施耐庵"
+    }, ]
+    return render_template("control.html", age=age, books=books)
+
+
+@app.route("/child1")
+def child1():
+    return render_template("child1.html")
+
+
+@app.route("/child2")
+def child2():
+    return render_template("child2.html")
+
+
+@app.route("/static")
+def static_demo():
+    return render_template("static.html")
 
 
 # 1. debug模式  app.run(debug=True)
